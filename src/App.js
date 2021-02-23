b182fb572e727ae00c031ae0fc70209d615af4b5
nnachevv@icloud.com <nnachevv@icloud.com>
2021-02-23 22:03:04 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+import "./App.css";
+import { Container } from "semantic-ui-react";
+import Login from "./Login";
+
+function App() {
+  return (
+    <div>
+      <Container>
+        <Login />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
