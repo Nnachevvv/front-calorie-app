2ed6ca5dd921895cba251e9b01884985f3623846
nnachevv@icloud.com <nnachevv@icloud.com>
2021-02-25 21:51:04 +0200

    Add two input forms and button

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Card, Header, Form, Input, Icon } from "semantic-ui-react";
+import {
+  Card,
+  Header,
+  Form,
+  Input,
+  Icon,
+  Button,
+  Message,
+} from "semantic-ui-react";
 
 let endpoint = "http://localhost:8080";
 
@@ -9,157 +17,91 @@ class Login extends Component {
     super(props);
 
     this.state = {
-      task: "",
-      items: []
+      username: "",
+      password: "",
+      items: [],
     };
   }
 
-  componentDidMount() {
-    this.getTask();
-  }
-
-  onChange = event => {
+  onChange = (event) => {
     this.setState({
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     });
   };
 
   onSubmit = () => {
-    let { task } = this.state;
-    // console.log("pRINTING task", this.state.task);
+    console.log("tuk", this.state.task);
+    let {task}  = this.state;
+    console.log(this.state)
     if (task) {
+      console.log(this.state)
+
       axios
         .post(
           endpoint + "/api/task",
           {
-            task
+            task,
           },
           {
             headers: {
-              "Content-Type": "application/x-www-form-urlencoded"
-            }
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
           }
         )
-        .then(res => {
-          this.getTask();
+        .then((res) => {
           this.setState({
-            task: ""
+            task: "",
           });
           console.log(res);
+
         });
     }
   };
 
-  getTask = () => {
-    axios.get(endpoint + "/api/task").then(res => {
-      console.log(res);
-      if (res.data) {
-        this.setState({
-          items: res.data.map(item => {
-            let color = "yellow";
-
-            if (item.status) {
-              color = "green";
-            }
-            return (
-              <Card key={item._id} color={color} fluid>
-                <Card.Content>
-                  <Card.Header textAlign="left">
-                    <div style={{ wordWrap: "break-word" }}>{item.task}</div>
-                  </Card.Header>
-
-                  <Card.Meta textAlign="right">
-                    <Icon
-                      name="check circle"
-                      color="green"
-                      onClick={() => this.updateTask(item._id)}
-                    />
-                    <span style={{ paddingRight: 10 }}>Done</span>
-                    <Icon
-                      name="undo"
-                      color="yellow"
-                      onClick={() => this.undoTask(item._id)}
-                    />
-                    <span style={{ paddingRight: 10 }}>Undo</span>
-                    <Icon
-                      name="delete"
-                      color="red"
-                      onClick={() => this.deleteTask(item._id)}
-                    />
-                    <span style={{ paddingRight: 10 }}>Delete</span>
-                  </Card.Meta>
-                </Card.Content>
-              </Card>
-            );
-          })
-        });
-      } else {
-        this.setState({
-          items: []
-        });
-      }
-    });
-  };
-
-  updateTask = id => {
+  updateTask = (id) => {
     axios
       .put(endpoint + "/api/task/" + id, {
         headers: {
-          "Content-Type": "application/x-www-form-urlencoded"
-        }
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
       })
-      .then(res => {
+      .then((res) => {
         console.log(res);
-        this.getTask();
       });
   };
 
-  undoTask = id => {
-    axios
-      .put(endpoint + "/api/undoTask/" + id, {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded"
-        }
-      })
-      .then(res => {
-        console.log(res);
-        this.getTask();
-      });
-  };
-
-  deleteTask = id => {
-    axios
-      .delete(endpoint + "/api/deleteTask/" + id, {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded"
-        }
-      })
-      .then(res => {
-        console.log(res);
-        this.getTask();
-      });
-  };
   render() {
     return (
       <div>
         <div className="row">
           <Header className="header" as="h2">
-            TO DO LIST
+            Login
           </Header>
         </div>
+
         <div className="row">
-          <Form onSubmit={this.onSubmit}>
             <Input
               type="text"
-              name="task"
+              name="username"
               onChange={this.onChange}
-              value={this.state.task}
+              value={this.state.username}
               fluid
-              placeholder="Create Task"
+              placeholder="Username"
             />
-            {/* <Button >Create Task</Button> */}
-          </Form>
         </div>
+
+        <div className="row">
+            <Input
+              type="text"
+              name="password"
+              onChange={this.onChange}
+              value={this.state.password}
+              fluid
+              placeholder="Password"
+            />
+        </div>
+        <Button color='teal' content='Login' icon='unlock' labelPosition='left' onClick={this.onSubmit} />
+
         <div className="row">
           <Card.Group>{this.state.items}</Card.Group>
         </div>
